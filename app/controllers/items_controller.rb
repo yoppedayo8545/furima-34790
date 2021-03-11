99262489d0b025667f6bcff706dab831ec385204
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-03-11 11:07:10 +0900

    商品情報編集機能(修正3)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user.id
+    redirect_to action: :index if current_user.id != @item.user.id
   end
 end
