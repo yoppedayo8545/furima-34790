1682eb9637a5de4958fdf15b549955e5657317fe
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-03-12 12:07:03 +0900

    "購入内容の確認"部分の実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,23 @@
+class PurchasesController < ApplicationController
+  def index
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_num).merge(user_id: current_user.id)
+  end
+
+end
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,17 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_num, :price, :user_id
+
+  with_options presence: true do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefectures_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    purchase = Purchase.create(price: price, user_id: user_id)
+    Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, phone_num: phone_num)
+  end
+  
+end
