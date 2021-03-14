7310c8aa76fde317f89adfd63b9c539144378bdb
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-03-14 18:26:20 +0900

    商品購入機能(修正6)

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -7,7 +7,7 @@ class PurchaseAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_num, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Numbers within 11 digits' }
   end
-  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefectures_id, numericality: { other_than: 1, message: "must be other than 1" }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
